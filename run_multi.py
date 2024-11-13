c981bb5b637fd2f047043c3a3f8d4ec506182479
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2024-11-14 01:34:49 +0700

    chore: add pickles

--- run_multi.py
+++ run_multi.py
@@ -13,6 +13,27 @@ node_map = pickle.load(open(file_path + ".map.pkl", "rb"))
 train = pickle.load(open(file_path + ".train.pkl", "rb"))
 test = pickle.load(open(file_path + ".test.pkl", "rb"))
 
+# print("node_list:", len(node_list))
+# print("adj_lists:", len(adj_lists.keys()))
+# print("rare_patient:", len(rare_patient))
+# print("labels:", len(labels))
+# print("node_map:", len(node_map.keys()))
+# print("train:", len(train))
+# print("test:", len(test))
+# print(rare_patient)
+# exit()
+
+# print(train)
+
+# print("sum in labels[len(labels)-1]:", sum(labels[len(labels)-1]))
+# Get labels that have more than 1 disease
+# multi_labels = []
+# for i in range(len(labels)):
+#     if sum(labels[i]) > 1:
+#         multi_labels.append(i)
+
+# print("multi_labels:", multi_labels)
+
 multi_class_num = 108
 feature_dim = 10000
 epoch = 8000
